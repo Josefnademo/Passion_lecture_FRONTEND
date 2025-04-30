f55b1ef0a04593225788cf206787884b33f4cc66
YOSEF NADEMO <72689921+Josefnademo@users.noreply.github.com>
2025-04-30 11:10:23 +0200

    docker✅

--- frontend/PassionLecture/src/components/Acceuil.vue
+++ frontend/PassionLecture/src/components/Acceuil.vue
@@ -6,4 +6,9 @@
     Passion Lecture is a site for book lovers who love reading and who know how to read books full
     of pages composed of sentences themselves composed of a string of diverse and varied words.
   </h2>
+  <div class="books">
+    <div class="book">
+      <img src="" alt="" />
+    </div>
+  </div>
 </template>
