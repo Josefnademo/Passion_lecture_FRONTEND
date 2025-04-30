42110350b05f26f24f8a13aa3f96a1d54e12ed86
C-HM <pc01zcg@eduvaud.ch>
2025-04-30 09:03:48 +0200

    feat(baseHeader)
    [DONE] [10min]
    - Ajout d'une base pour le header

---
+++ frontend/PassionLecture/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <div>
+      <button>☰</button>
+      <h1>PASSION LECTURE</h1>
+    </div>
+    <div>
+      <button>👤</button>
+    </div>
+  </header>
+</template>
+
+<script></script>
+
+<style scoped>
+header {
+  background-color: #1a3c5a;
+  justify-items: center;
+}
+</style>
