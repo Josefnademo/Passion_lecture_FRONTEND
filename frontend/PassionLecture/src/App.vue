ec980d5f0ed4c8596254529e40096e93196b8443
YOSEF NADEMO <72689921+Josefnademo@users.noreply.github.com>
2025-04-30 10:20:31 +0200

    figma footer[En]
    [5min] [DONE]
    - version anglaise

--- frontend/PassionLecture/src/App.vue
+++ frontend/PassionLecture/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Header></Header>
     <RouterView />
     <Footer></Footer>
   </div>
--- frontend/PassionLecture/src/router/index.js
+++ frontend/PassionLecture/src/router/index.js
@@ -12,9 +12,8 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      // Dynamic import creates a separate file (chunk)
+      // which will be (lazy-loaded)loaded only when this route is visited
       component: () => import('../views/AboutView.vue'),
     },
   ],
